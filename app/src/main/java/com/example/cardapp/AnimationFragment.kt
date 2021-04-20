199a59cf87912d44483bd63d9a52b67e73be4dd6
david-storm <venia7vedmak@gmail.com>
2021-04-20 19:02:49 +0300

    add animation and modelView to two fragments

--- app/src/main/java/com/example/cardapp/AnimationFragment.kt
+++ app/src/main/java/com/example/cardapp/AnimationFragment.kt
@@ -1,12 +1,20 @@
 package com.example.cardapp
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator.INFINITE
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
 import com.example.cardapp.databinding.FragmentAnimationBinding
 import com.example.cardapp.databinding.FragmentCustomizationBinding
@@ -14,7 +22,7 @@ import com.example.cardapp.databinding.FragmentCustomizationBinding
 class AnimationFragment : Fragment() {
 
 
-    private lateinit var viewModel: CustomizationViewModel
+    private val viewModel: CustomizationViewModel by activityViewModels()
     private lateinit var binding: FragmentAnimationBinding
 
     override fun onCreateView(
@@ -25,20 +33,63 @@ class AnimationFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_animation,container,false)
 
-        viewModel = ViewModelProvider(this).get(CustomizationViewModel::class.java)
-
         binding.customizationViewModel = viewModel
 
-//        binding.background.setBackgroundResource(viewModel.background)
-//        Log.i("animation", viewModel.background.toString())
+        binding.item.setImageResource(viewModel.item.value!!)
 
         animation()
         return binding.root
     }
 
     private fun animation(){
-
-
+        fadeBackground()
+        rotateItem()
+        translateAvatar()
+        translateText()
     }
 
+    private fun fadeBackground() {
+        binding.background.setBackgroundResource(viewModel.background.value!!)
+        val animator = ObjectAnimator.ofFloat(binding.background, View.ALPHA, 0f, 1f)
+        animator.duration = 2000
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                fadeTitle()
+            }
+        })
+        animator.start()
+    }
+
+    private fun rotateItem(){
+        val animator = ObjectAnimator.ofFloat(binding.item, View.ROTATION, 0f, 360f)
+        animator.interpolator = LinearInterpolator()
+        animator.duration = 1000
+        animator.repeatCount = INFINITE
+        animator.start()
+    }
+
+    private fun fadeTitle(){
+        val animator = ObjectAnimator.ofFloat(binding.title, View.ALPHA, 0f)
+        animator.repeatCount = 15
+        animator.duration = 100
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
+    }
+    private fun translateAvatar() {
+        val animator = ObjectAnimator.ofFloat(binding.avatar, View.TRANSLATION_Y, 500f)
+        animator.repeatCount = 1
+        animator.duration = 5000
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.start()
+    }
+
+    private fun translateText() {
+        val length =  binding.text.getWidth()
+        val width = binding.background.getWidth()
+        Log.i("translate", (width + length).toFloat().toString())
+        Log.i("translate", (-length).toFloat().toString())
+        val animator = ObjectAnimator.ofFloat(binding.text, View.TRANSLATION_X, width + length.toFloat(), -length.toFloat())
+        animator.duration = 5000
+        animator.start()
+    }
 }
--- app/src/main/java/com/example/cardapp/CustomizationFragment.kt
+++ app/src/main/java/com/example/cardapp/CustomizationFragment.kt
@@ -1,22 +1,21 @@
 package com.example.cardapp
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.cardapp.databinding.FragmentCustomizationBinding
 
 
 class CustomizationFragment : Fragment() {
 
-    private lateinit var viewModel: CustomizationViewModel
+    private val viewModel: CustomizationViewModel by activityViewModels()
     private lateinit var binding: FragmentCustomizationBinding
 
     override fun onCreateView(
@@ -27,9 +26,6 @@ class CustomizationFragment : Fragment() {
             inflater,
             R.layout.fragment_customization, container, false
         )
-
-        viewModel = ViewModelProvider(this).get(CustomizationViewModel::class.java)
-
 
         binding.fieldNameEdit.setOnFocusChangeListener { _, hasFocus ->
             if (!hasFocus)
@@ -53,20 +49,24 @@ class CustomizationFragment : Fragment() {
         })
 
         binding.buttonTest.setOnClickListener{ view: View ->
-            view.findNavController().navigate(R.id.action_customizationFragment_to_animationFragment)
+            viewModel.text = binding.fieldTextEdit.text.toString()
+            viewModel.title = binding.fieldTitleEdit.text.toString()
+            viewModel.name = binding.fieldNameEdit.text.toString()
+
+            if (viewModel.validationField())
+                view.findNavController()
+                    .navigate(R.id.action_customizationFragment_to_animationFragment)
+             else
+                viewMessageError()
+        }
+
+
+        binding.buttonLaunch.setOnClickListener { view: View ->
+
         }
 
         binding.customizationViewModel = viewModel
 
-        binding.buttonLaunch.setOnClickListener { view: View ->
-            if (viewModel.validationField()) {
-                view.findNavController()
-                    .navigate(R.id.action_customizationFragment_to_animationFragment)
-            } else {
-                viewMessageError()
-            }
-
-        }
         return binding.root
     }
 
